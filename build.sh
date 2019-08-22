33a7e8e8ccd0638d31502445ef815404dea8c0f7
Lucas Desouza <lucasfds2777>
2019-08-21 21:36:25 -0500

    fix error in push

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ docker build -t lucasfds/breath:latest -t lucasfds/breath:$1 .
 
 #upload image
 docker push lucasfds/breath:latest
-docker push lucasfds/breath:latest:$1
+docker push lucasfds/breath:$1
