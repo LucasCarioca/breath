1555972c891859da328c7032abfc2a9a09374616
Lucas Desouza <lucasfds2777@gmail.com>
2020-05-31 17:57:02 -0500

    fix: styling for legal links section

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import './App.css';
-import { createStore } from "redux";
+import {createStore} from "redux";
 import reducer from "./store/reducer";
-import { Provider } from "react-redux";
-import { composeWithDevTools } from "redux-devtools-extension";
+import {Provider} from "react-redux";
+import {composeWithDevTools} from "redux-devtools-extension";
 import BottomNavigation from "@material-ui/core/BottomNavigation";
 import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
 import RestoreIcon from '@material-ui/icons/Restore';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
-import { makeStyles } from "@material-ui/core/styles";
-import { BrowserRouter as Router, Link } from "react-router-dom";
+import {makeStyles} from "@material-ui/core/styles";
+import {BrowserRouter as Router, Link} from "react-router-dom";
 import Routes from "./Routes";
 import Container from "@material-ui/core/Container";
 import Box from "@material-ui/core/Box";
 import AppBar from "@material-ui/core/AppBar";
-import { Button } from '@material-ui/core';
+import {Button, Card} from '@material-ui/core';
+import CardContent from "@material-ui/core/CardContent";
 
 const store = createStore(reducer, composeWithDevTools());
 
@@ -41,11 +42,19 @@ function App() {
             <Router>
                 <Box height={'100%'}>
                     <Container maxWidth={'sm'}>
-                        <Routes />
-                        <div className='legal-links-container'>
-                            <Button className='legal-links' component={Link} size={'small'} color={'default'} to='/privacypolicy'>Privacy policy</Button>
-                            <Button className='legal-links' component={Link} size={'small'} color={'default'} to='/termsofservice'>Terms of service</Button>
-                        </div>
+                        <Routes/>
+                        <br/>
+                        <Card raised={true} className={'legal-links-card'}>
+                            <CardContent>
+                                <p>Copyright &copy; 2020 Lucas Desouza</p>
+                                <div className='legal-links-container'>
+                                    <Button className='legal-links' component={Link} size={'small'} color={'default'}
+                                            to='/privacypolicy'>Privacy policy</Button>
+                                    <Button className='legal-links' component={Link} size={'small'} color={'default'}
+                                            to='/termsofservice'>Terms of service</Button>
+                                </div>
+                            </CardContent>
+                        </Card>
                         <AppBar>
                             <BottomNavigation
                                 value={value}
@@ -55,9 +64,12 @@ function App() {
                                 showLabels
                                 className={classes.root}
                             >
-                                <BottomNavigationAction label="Counter" icon={<FavoriteIcon />} component={Link} to={'/'} />
-                                <BottomNavigationAction label="Recents" icon={<RestoreIcon />} component={Link} to={'/record'} />
-                                <BottomNavigationAction label="Account" icon={<AccountCircleIcon />} component={Link} to={'/account'} />
+                                <BottomNavigationAction label="Counter" icon={<FavoriteIcon/>} component={Link}
+                                                        to={'/'}/>
+                                <BottomNavigationAction label="Recents" icon={<RestoreIcon/>} component={Link}
+                                                        to={'/record'}/>
+                                <BottomNavigationAction label="Account" icon={<AccountCircleIcon/>} component={Link}
+                                                        to={'/account'}/>
                             </BottomNavigation>
                         </AppBar>
                     </Container>
