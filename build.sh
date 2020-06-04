78eea7855dc9e96a083ad56637e989066ee0af74
Lucas Desouza <lucasfds2777@gmail.com>
2020-06-04 17:33:18 -0500

    cleans up old content

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-#build and tag image
-docker build -t lucasfds/breath:latest -t lucasfds/breath:$1 . 
-
-#upload image
-docker push lucasfds/breath:latest
-docker push lucasfds/breath:$1
--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-kubectl apply -f ./k8s
-
-kubectl set image deployments/breath-deployment breath=lucasfds/breath:$1
