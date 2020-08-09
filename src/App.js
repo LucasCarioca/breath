76d20ca62418accd68c86d3609829c5548a521e6
Lucas Desouza <lucasfds2777@gmail.com>
2020-08-09 12:54:44 -0500

    feat(app-links): adds google play link

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import './App.css';
-import {createStore} from "redux";
+import { createStore } from "redux";
 import reducer from "./store/reducer";
-import {Provider} from "react-redux";
-import {composeWithDevTools} from "redux-devtools-extension";
+import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import BottomNavigation from "@material-ui/core/BottomNavigation";
 import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
 import RestoreIcon from '@material-ui/icons/Restore';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
-import {makeStyles} from "@material-ui/core/styles";
-import {BrowserRouter as Router, Link} from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 import Routes from "./Routes";
 import Container from "@material-ui/core/Container";
 import Box from "@material-ui/core/Box";
 import AppBar from "@material-ui/core/AppBar";
-import {Button, Card} from '@material-ui/core';
+import { Button, Card } from '@material-ui/core';
 import CardContent from "@material-ui/core/CardContent";
 import AppStoreSVG from "./assets/appstore.svg"
+import GooglePlayPNG from "./assets/google-play-badge.png"
 
 const store = createStore(reducer, composeWithDevTools());
 
@@ -43,19 +44,25 @@ function App() {
             <Router>
                 <Box height={'100%'}>
                     <Container maxWidth={'sm'}>
-                        <Routes/>
-                        <br/>
+                        <Routes />
+                        <br />
                         <Card raised={true} className={'legal-links-card'}>
                             <CardContent>
-                                <a href="https://apps.apple.com/us/app/count-my-breaths/id1513102381">
-                                    <img src={AppStoreSVG} alt="Download on the Apple app store"/>
-                                </a>
+                                <p>This web app is outdated. Please use one of our mobile options available through the links below.</p>
+                                <div style={{display: 'flex', justifyContent: 'space-around'}}>
+                                    <a style={{margin: 'auto'}} href="https://apps.apple.com/us/app/count-my-breaths/id1513102381">
+                                        <img style={{ height: '40px' }} src={AppStoreSVG} alt="Download on the Apple app store" />
+                                    </a>
+                                    <a style={{margin: 'auto'}} href="https://play.google.com/store/apps/details?id=net.lucasdesouza.countmybreaths">
+                                        <img style={{ height: '60px' }} src={GooglePlayPNG} alt="Download on the Google Play store" />
+                                    </a>
+                                </div>
                                 <p>Copyright &copy; 2020 Lucas Desouza</p>
                                 <div className='legal-links-container'>
                                     <Button className='legal-links' component={Link} size={'small'} color={'secondary'}
-                                            to='/privacypolicy'>Privacy policy</Button>
+                                        to='/privacypolicy'>Privacy policy</Button>
                                     <Button className='legal-links' component={Link} size={'small'} color={'secondary'}
-                                            to='/termsofservice'>Terms of service</Button>
+                                        to='/termsofservice'>Terms of service</Button>
                                 </div>
                             </CardContent>
                         </Card>
@@ -68,12 +75,12 @@ function App() {
                                 showLabels
                                 className={classes.root}
                             >
-                                <BottomNavigationAction label="Counter" icon={<FavoriteIcon/>} component={Link}
-                                                        to={'/'}/>
-                                <BottomNavigationAction label="Recents" icon={<RestoreIcon/>} component={Link}
-                                                        to={'/record'}/>
-                                <BottomNavigationAction label="Account" icon={<AccountCircleIcon/>} component={Link}
-                                                        to={'/account'}/>
+                                <BottomNavigationAction label="Counter" icon={<FavoriteIcon />} component={Link}
+                                    to={'/'} />
+                                <BottomNavigationAction label="Recents" icon={<RestoreIcon />} component={Link}
+                                    to={'/record'} />
+                                <BottomNavigationAction label="Account" icon={<AccountCircleIcon />} component={Link}
+                                    to={'/account'} />
                             </BottomNavigation>
                         </AppBar>
                     </Container>
